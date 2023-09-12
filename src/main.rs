e571fd694675cfce72192b6a725cc87fec6e6cb7
stormckey <sortygraph@gmail.com>
2023-09-13 00:08:29 +0800

    init and add mini_redis

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use axum::{
+    Router,
+    routing::get,
+};
+
+#[tokio::main]
+async fn main(){
+    let app = Router::new().route("/", get(|| async {"Hello, World!"}));
+
+    axum::Server::bind(
+        &"0.0.0.0:3000".parse().unwrap()
+    ).serve(app.into_make_service()).await.unwrap();
+}
